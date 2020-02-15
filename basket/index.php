b12f1faa9a0467557a8f7f6fe4dc3426da045356
Maëlle D. - BTS SIO <Malle D. - BTS SIO@PC-211>
2020-02-15 13:24:34 +0100

    MAJ de test

--- basket/index.php
+++ basket/index.php
@@ -1,17 +1,18 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+
+<?php
+session_start();
+include 'html/header.php';
+//include 'connexion.php';
+
+$action='';
+if(isset($_GET['action'])){
+    $action=$_GET['action'];
+}
+if($action==''){
+    include 'html/acceuil/accueil.php';
+}
+//elseif($action=='exercice1'){
+//    include 'exercice1.php';
+//}
+include 'html/footer.php';
+?>
