0bba731a5b550a3d7c3bb5551fb4cf4fd5231f57
Maëlle D. - BTS SIO <Malle D. - BTS SIO@PC-211>
2020-02-20 12:14:13 +0100

    Add : - 20/02/20 12:10 - Lien mysql réussi sur le fichier creer_sponsor ! - Lien mysql en cours pour creer_article !

--- basket/html/gestion_admin/article/creer_article.php
+++ basket/html/gestion_admin/article/creer_article.php
@@ -1,5 +1,71 @@
-<h1>Page de création d'article</h1>
-<label>Titre* : </label><input type="text"/>
-<label>Ville* : </label><input type="text"/>
-<label>Departement : </label><input type="text"/>
-<label>Description* : </label><input type="text"/>
+<?php
+$article = '';
+$dateArticle = '';
+$titre = '';
+$descriptionArticle = '';
+$imageArticle = '';
+$villeArticle = '';
+$departement = '';
+$message = '';
+$donneeErreur = '';
+$valide = false;
+
+if (isset($_POST['nom'])) {
+    $nom = $_POST["nom"];
+    $adresse = $_POST["adresse"];
+    $ville = $_POST["ville"];
+    $codePostal = intval($_POST["codePostal"]);
+    $telephone = intval($_POST["telephone"]);
+    $lien = ($_POST["lien"]);
+    $description = ($_POST["description"]);
+    $image = $_POST["image"];
+
+    if ((strlen($codePostal)) != 5) {
+        $donneeErreur = $donneeErreur . "Code postal invalide,<br>";
+        $valide = false;
+    }
+    if (((strlen($telephone)) > 15) || (strlen($telephone)) < 10) {
+        $donneeErreur = $donneeErreur . "Numero de téléphone invalide, <br>";
+    }
+    if (((strlen($nom)) > 70) || (strlen($nom)) < 1) {
+        $donneeErreur = $donneeErreur . "Nom invalide, <br>";
+    }
+    if (((strlen($adresse)) > 50) || (strlen($adresse)) < 1) {
+        $donneeErreur = $donneeErreur . "Adresse invalide, <br>";
+    }
+    if (((strlen($ville)) > 50) || (strlen($ville)) < 1) {
+        $donneeErreur = $donneeErreur . "Ville invalide, <br>";
+    }
+    if (((strlen($lien)) > 70) || (strlen($lien)) < 1) {
+        $donneeErreur = $donneeErreur . "Lien sponsor invalide, <br>";
+    }
+    if (((strlen($description)) > 750) || (strlen($lien)) < 1) {
+        $donneeErreur = $donneeErreur . "Description invalide, <br>";
+    }
+    if (((strlen($image)) > 750) || (strlen($image)) < 1) {
+        $donneeErreur = $donneeErreur . "Image invalide, <br>";
+    } else {
+        $requete = "insert into sponsor (nom,adresse,ville,codePostal,telephone,lien,description,image) values ('$nom','$adresse','$ville',$codePostal,$telephone,'$lien','$description','$image');";
+        echo $requete;
+        $connexion->exec($requete);
+        $message = 'Traitement effectué';
+    }
+    if ($donneeErreur != '') {
+        $message = "Erreur,<br>" . $donneeErreur;
+    }
+}
+?>
+<form  name="monForm" method="post" action="index.php?action=creer_sponsor" >
+    <h1>Page de création d'article</h1>
+    <p>Titre* : <input type="text" name="nom" value='<?= $nom ?>'  ></p>
+    <p>Ville* : <input type="text" name="nom" value='<?= $nom ?>'  ></p>
+    <p>Departement : <input type="text" name="nom" value='<?= $nom ?>'  ></p>
+    <p>Description* : <input type="text" name="nom" value='<?= $nom ?>'  ><p/>
+    <br>
+    <br>
+    <strong><?= $message ?></strong>
+    <div>
+        <input type='submit' value='Enregistrer' />
+    </div>
+    <br>
+</form>
--- basket/html/gestion_admin/sponsor/creer_sponsor.php
+++ basket/html/gestion_admin/sponsor/creer_sponsor.php
@@ -7,6 +7,9 @@ $telephone = '';
 $lien = '';
 $description = '';
 $image = '';
+$message = '';
+$donneeErreur = '';
+$valide=false;
 
 if (isset($_POST['nom'])) {
     $nom = $_POST["nom"];
@@ -17,28 +20,59 @@ if (isset($_POST['nom'])) {
     $lien = ($_POST["lien"]);
     $description = ($_POST["description"]);
     $image = $_POST["image"];
+
+    if ((strlen($codePostal)) != 5) {
+        $donneeErreur = $donneeErreur . "Code postal invalide,<br>";
+        $valide=false;
+    }
+    if (((strlen($telephone)) > 15) || (strlen($telephone)) < 10) {
+        $donneeErreur = $donneeErreur . "Numero de téléphone invalide, <br>";
+    }
+    if (((strlen($nom)) > 70) || (strlen($nom)) < 1) {
+        $donneeErreur = $donneeErreur . "Nom invalide, <br>";
+    }
+    if (((strlen($adresse)) > 50) || (strlen($adresse)) < 1) {
+        $donneeErreur = $donneeErreur . "Adresse invalide, <br>";
+    }
+    if (((strlen($ville)) > 50) || (strlen($ville)) < 1) {
+        $donneeErreur = $donneeErreur . "Ville invalide, <br>";
+    }
+    if (((strlen($lien)) > 70) || (strlen($lien)) < 1) {
+        $donneeErreur = $donneeErreur . "Lien sponsor invalide, <br>";
+    }
+    if (((strlen($description)) > 750) || (strlen($lien)) < 1) {
+        $donneeErreur = $donneeErreur . "Description invalide, <br>";
+    }
+    if (((strlen($image)) > 750) || (strlen($image)) < 1) {
+        $donneeErreur = $donneeErreur . "Image invalide, <br>";
+    }
     
-    $requete = "insert into sponsor (nom,adresse,ville,codePostal,telephone,lien,description,image) values ('$nom','$adresse','$ville',$codePostal,$telephone,'$lien','$description','$image');";
-    echo $requete;
-    $connexion->exec($requete);
-    $message = 'Traitement effectué';
-} else {
-    $message = "Erreur";
+    else {
+        $requete = "insert into sponsor (nom,adresse,ville,codePostal,telephone,lien,description,image) values ('$nom','$adresse','$ville',$codePostal,$telephone,'$lien','$description','$image');";
+        echo $requete;
+        $connexion->exec($requete);
+        $message = 'Traitement effectué';
+    }
+    if ($donneeErreur != '') {
+        $message = "Erreur,<br>" . $donneeErreur;
+    }
 }
 ?>
 <form  name="monForm" method="post" action="index.php?action=creer_sponsor" >
     <h1>Création d'un sponsor</h1>
-    <p>Nom* : <input type="text" name="nom" ></p>
-    <p>Adresse* : <input type="text" name="adresse"  ></p>
-    <p>Ville* : <input type="text" name="ville" ></p>
-    <p>Code Postal* : <input type="text" name="codePostal"  ></p>
-    <p>Téléphone* : <input type="text" name="telephone" </p>
-    <p>Lien sponsor* : <input type="text" name="lien" </p>
-    <p>Description* : <input type="text" name="description"></p>
-    <p>Lien image : <input type="text" name="image" </p>
+    <p>Nom* : <input type="text" name="nom" value='<?= $nom ?>'  ></p>
+    <p>Adresse* : <input type="text" name="adresse" value='<?= $adresse ?>'   ></p>
+    <p>Ville* : <input type="text" name="ville" value='<?= $ville ?>'  ></p>
+    <p>Code Postal* : <input type="text" name="codePostal" value='<?= $codePostal ?>'   ></p>
+    <p>Téléphone* : <input type="text" name="telephone" value='<?= $telephone ?>' </p>
+    <p>Lien sponsor* : <input type="text" name="lien" value='<?= $lien ?>'  </p>
+    <p>Description* : <input type="text" name="description" value='<?= $description ?>' ></p>
+    <p>Lien image : <input type="text" name="image" value='<?= $image ?>'  </p>
+    <br>
+    <br>
+    <strong><?= $message ?></strong>
     <div>
         <input type='submit' value='Enregistrer' />
     </div>
-    <strong><?= $message ?></strong>
     <br>
 </form>
