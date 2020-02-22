9375c560d44782cd379e9096742b2be391dab3c2
GaboritJunior <juniorgaborit@gmail.com>
2020-02-22 18:41:37 +0100

    Add : - 22/02/20 18:41
    - Correction du bug de l'image dans gestion_sponsors

--- basket/html/gestion_admin/sponsor/gestion_sponsor.php
+++ basket/html/gestion_admin/sponsor/gestion_sponsor.php
@@ -1,1 +1,14 @@
 <h1>Gestion sponsors</h1>
+<?php
+    include 'connexion.php';
+    $requete = "select * from sponsor";
+    //Exécution de  la requête qui renvoie le résultat dans  $resultats, 
+    $resultats = $connexion->query($requete);
+    echo "<h2>Listes des sponsors</h2>";
+    //On récupère toutes les lignes de la table dans la variable $lignes qui est un tableau associatif
+    $lignes = $resultats->fetchALL(PDO::FETCH_ASSOC);
+    foreach ($lignes as $ligne) {
+        //on affiche la ligne qu'on vient de lire
+        echo $ligne['idSponsor']," ",$ligne['nom']," ",$ligne['adresse']," ",$ligne['ville']," ",$ligne['codePostal']," ",$ligne['telephone']," ",$ligne['lien']," ",$ligne['description']," ","<img src='http://localhost/basket/basket/images/".$ligne['image']."'>"," <br/>\n";
+    }
+?>
