b66e2673bfa201cde1f1c9684bdc895b432e9947
GaboritJunior <juniorgaborit@gmail.com>
2020-02-29 14:07:03 +0100

    Add : - 29/02/20 14:06
    - Fonctionnement parfait de gestion_action
    - Fonctionnement parfait de creation_action
    - implentation des boutons de creation / modification / suppression a gesion_action
    - retrait du bouton de modification et de creation des action au header

--- basket/html/gestion_admin/action/creer_action.php
+++ basket/html/gestion_admin/action/creer_action.php
@@ -1,18 +1,52 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <h1>Page de création d'action</h1>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+$idArticle=0;
+$idSponsor=0;
+$typeDon='';
+$montant=0;
+$message = '';
+$donneeErreur = '';
+$valide = false;
+
+if (isset($_POST['idArticle'])) {
+    $idArticle = $_POST["idArticle"];
+    $idSponsor = $_POST["idSponsor"];
+    $typeDon = $_POST["typeDon"];
+    $montant = $_POST["montant"];
+
+    if ((strlen($montant)) < 1) {
+        $donneeErreur = $donneeErreur . "Montant invalide,<br>";
+    }
+    if ((strlen($idArticle)) < 1) {
+        $donneeErreur = $donneeErreur . "Id Article invalide,<br>";
+    }
+    if ((strlen($idSponsor)) < 1) {
+        $donneeErreur = $donneeErreur . "Id Sponsor invalide,<br>";
+    }
+    if (((strlen($typeDon)) > 20) || (strlen($typeDon)) < 1) {
+        $donneeErreur = $donneeErreur . "Numero de téléphone invalide, <br>";
+    }
+    if ($donneeErreur != '') {
+        $message = "Erreur,<br>" . $donneeErreur;
+    }
+    if ($donneeErreur==''){
+        $requete = "insert into action (idArticle,idSponsor,typeDon,montant,date) values ('$idArticle','$idSponsor','$typeDon',$montant,NOW());";
+        echo $requete;
+        $connexion->exec($requete);
+        $message = 'Traitement effectué';
+    }
+}
+?>
+<form  name="monForm" method="post" action="index.php?action=creer_action" >
+    <h1>Création d'une action</h1>
+    <p>ID Article* : <input type="text" name="idArticle" value='<?= $idArticle ?>'  ></p>
+    <p>ID Sponsor* : <input type="text" name="idSponsor" value='<?= $idSponsor ?>'   ></p>
+    <p>Type de don* : <input type="text" name="typeDon" value='<?= $typeDon ?>'  ></p>
+    <p>Montant* : <input type="text" name="montant" value='<?= $montant ?>'   ></p>
+    <br>
+    <br>
+    <strong><?= $message ?></strong>
+    <div>
+        <input type='submit' value='Enregistrer' />
+    </div>
+    <br>
+</form>
--- basket/html/gestion_admin/action/gestion_action.php
+++ basket/html/gestion_admin/action/gestion_action.php
@@ -2,3 +2,28 @@
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" OnClick="window.location.href='index.php?action=creer_action'">
     <span class="title">Créer des actions</span>
 </button>
+<?php
+    $supprime='';
+    $idArticle=0;
+    $idSponsor=0;
+    if (isset($_GET['supprime'])){
+        $idSponsor=$_GET['idSponsor'];
+        $idSponsor=$_GET['idArticle'];
+        $requete="delete from sponsor where idSponsor='$idSponsor' and idArticle='$idArticle'";
+        echo $requete;
+    $connexion->exec($requete);
+    }
+    include 'connexion.php';
+    $requete = "select * from action";
+    //Exécution de  la requête qui renvoie le résultat dans  $resultats, 
+    $resultats = $connexion->query($requete);
+    echo "<h2>Listes des sponsors</h2>";
+    //On récupère toutes les lignes de la table dans la variable $lignes qui est un tableau associatif
+    $lignes = $resultats->fetchALL(PDO::FETCH_ASSOC);
+    foreach ($lignes as $ligne) {
+        //on affiche la ligne qu'on vient de lire
+        echo "<p>",$ligne['typeDon']," ",$ligne['montant']," ",$ligne['date']," "
+        ,"<button class='navbar-toggler' type='button' data-toogle='collapse' data-target='#navbarToggleExternalContent' aria-controls='navbarToggleExternalContent' aria-expanded='false' aria-label='Toggle navigation' ","OnClick=window.location.href="."'index.php?action=modifier_action&idSponsor={$ligne['idSponsor']}&idArticle={$ligne['idArticle']}'"."><span class='title'>Modifier</span></button>"
+        ,"<button class='navbar-toggler' type='button' data-toogle='collapse' data-target='#navbarToggleExternalContent' aria-controls='navbarToggleExternalContent' aria-expanded='false' aria-label='Toggle navigation' ","OnClick=window.location.href="."'index.php?action=gestion_sponsor&idSponsor={$ligne['idSponsor']}&idArticle={$ligne['idArticle']}&supprime=ok'"."><span class='title'>Supprimer</span></button></p>";
+    }
+?>
