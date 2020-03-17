340f6b29338d6168f129a80e2208d0c4938135c4
GaboritJunior <juniorgaborit@gmail.com>
2020-03-17 08:43:36 +0100

    Add : - 17/03/20 08:42
    - ajout des dossier action et articles dans js
    - ajout de script js dans js/action , js/article et js/sponsors ( creer / modifier )

--- basket/html/gestion_admin/action/creer_action.php
+++ basket/html/gestion_admin/action/creer_action.php
@@ -1,4 +1,10 @@
 <?php
+$requete = "select * from action ";
+//Exécution de  la requête qui renvoie le résultat dans  $resultats, 
+$resultats = $connexion->query($requete);
+//On récupère toutes les lignes de la table dans la variable $lignes qui est un tableau associatif
+$lignes = $resultats->fetchALL(PDO::FETCH_ASSOC);
+
 $idArticle = 0;
 $idSponsor = 0;
 $typeDon = '';
